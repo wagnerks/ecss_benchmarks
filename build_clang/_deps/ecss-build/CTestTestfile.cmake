a6e49a8c74f52d2ae376da90347a44299b678385
kirill vagner <kirill.vagner@playrix.com>
2025-12-14 23:03:36 +0100

    Revert "Fix CI: increase timeouts, use MinTime from code"
    This reverts commit a4675ff21cbe3f46be6ceeaca9d496f47833e8d1.

--- build_clang/_deps/ecss-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/projects/pet/ecss_benchmarks/build_clang/_deps/ecss-src
-# Build directory: D:/projects/pet/ecss_benchmarks/build_clang/_deps/ecss-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
